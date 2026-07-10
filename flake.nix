78fa20cbf0da444a082d936b56b63f9e98175e9a
Karl Skewes <hello@karlskewes.com>
2026-07-10 19:01:49 +1000

    feat: set version to 1.5.0-dev

--- flake.nix
+++ flake.nix
@@ -87,7 +87,7 @@
 
           cosmic-comp = rustPlatform.buildRustPackage {
             pname = "cosmic-comp";
-            version = "1.0.0-dev";
+            version = "1.5.0-dev";
 
             src = common.pkgs.lib.fileset.toSource {
               root = ./.;
